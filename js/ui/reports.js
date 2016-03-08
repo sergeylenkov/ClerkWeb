c67bcbfbf131a54d4635356e4d6af6e3913d4aaf
Sergey Lenkov <serglazy@gmail.com>
2016-03-08 21:56:26 +0300

    replace bar chart with line graph

--- js/ui/reports.js
+++ js/ui/reports.js
@@ -55,10 +55,53 @@ function Reports() {
         var max = d3.max(data, function(d) { return d.value; });
         var scale = d3.scale.linear().domain([0, max]).range([0, height]);
         var svg = d3.select("#reports_canvas").attr("width", width).attr("height", height);
-        var tooltip = d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
+        //var tooltip = d3.select("body").append("div").attr("class", "tooltip").style("opacity", 0);
 
         svg.selectAll("*").remove();
-        svg.selectAll("rect").data(data).enter().append("rect")
+
+        var path = svg.append("g").attr("transform", "translate(" + 60 + "," + -30 + ")");
+        var parseDate = d3.time.format("%Y %m").parse;
+
+        // Set the ranges
+        var x = d3.time.scale().range([0, width - 80]);
+        var y = d3.scale.linear().range([height, 40]);
+
+        // Define the axes
+        var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(5);
+
+        var yAxis = d3.svg.axis().scale(y).orient("left").ticks(5);
+
+        // Define the line
+        var valueline = d3.svg.line()
+            .x(function(d) { return x(d.date); })
+            .y(function(d) { return y(d.value); });
+
+        data.forEach(function(d) {
+            d.date = parseDate(d.date);
+            //d.value = +d.value;
+        });
+
+        // Scale the range of the data
+        x.domain(d3.extent(data, function(d) { return d.date; }));
+        y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+        // Add the valueline path.
+        path.append("path")
+            .attr("class", "line")
+            .attr("d", valueline(data));
+
+        // Add the X Axis
+        path.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+
+        // Add the Y Axis
+        path.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+
+        /*svg.selectAll("rect").data(data).enter().append("rect")
         .attr("x", function(d, i) {
             return i * (width / data.length);
         })
@@ -81,7 +124,7 @@ function Reports() {
         })
         .on("mouseout", function() {
             tooltip.style("opacity", 0);
-        });
+        });*/
     }
 
     this.updateAccountsList = function() {
