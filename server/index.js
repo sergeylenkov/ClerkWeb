618d1cf731d05865a1b741c6215dee361aefc2ba
Sergey Lenkov <sergey.lenkov@3d4medical.com>
2019-01-23 14:00:37 +0300

    Added API unit tests

--- server/index.js
+++ server/index.js
@@ -23,3 +23,5 @@ const port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log('App is listening on port ' + port);
+
+module.exports = app;
---
+++ server/test/api.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const request = require('supertest');
+const app = require('../index');
+
+describe('GET /accounts', () => {
+    it('should get all accounts', (done) => {
+      request(app)
+        .get('/accounts')
+        .expect(200)
+        .end(function(err, res) {
+          assert( res.body.items.length > 0, 'must be more then 0');
+          done();
+        });
+    });
+});
+
+describe('GET /dashboard/balance', () => {
+  it('should get balance', (done) => {
+    request(app)
+      .get('/dashboard/balance')
+      .expect(200)
+      .end(function(err, res) {        
+        assert( res.body.items.length > 0, 'must be more then 0');
+        done();
+      });
+  });
+});
