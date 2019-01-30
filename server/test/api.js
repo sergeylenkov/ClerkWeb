d426f5f221f8c0bff274b9486e9bcc7e689c149c
Sergey Lenkov <sergey.lenkov@3d4medical.com>
2019-01-30 17:21:13 +0300

    Added transactions test

--- server/test/api.js
+++ server/test/api.js
@@ -14,6 +14,18 @@ describe('GET /accounts', () => {
     });
 });
 
+describe('GET /transactions', () => {
+    it('should get all transactions', (done) => {
+      request(app)
+        .get('/transactions')
+        .expect(200)
+        .end(function(err, res) {
+          assert( res.body.items.length > 0, 'must be more then 0');
+          done();
+        });
+    });
+});
+
 describe('GET /dashboard/balance', () => {
   it('should get balance', (done) => {
     request(app)
