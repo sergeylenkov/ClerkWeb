a660fade05421490143018654055a9d23f105409
Sergey Lenkov <serglazy@gmail.com>
2019-03-23 17:53:39 +0300

    Dashboard budgets view changes

--- client/src/components/pages/dashboard/Budgets.js
+++ client/src/components/pages/dashboard/Budgets.js
@@ -11,11 +11,38 @@ export class DashboardBudgets extends React.Component {
 
                 {
                     this.props.budgets.map((item, i) => {
+                        const total = item.amount;
+                        const current = item.expense;
+                        const remains = total - current;
+                        let percent = current / (total / 100);
+
+                        if (percent > 100) {
+                            percent = 100;
+                        }
+
+                        const progressStyle = {
+                            width: `${percent}%`                            
+                        }
+
+                        let className = styles.item;
+
+                        if (percent > 50 && percent < 90) {
+                            className += ` ${styles.half}`;
+                        } else if (percent >= 90) {
+                            className += ` ${styles.over}`;
+                        }
+
                         return (
-                            <div key={item.id} className={styles.item}>
-                                <div className={styles.name}>{item.name}</div>
-                                <div className={styles.expense}>{formatAmount(item.expense, item.currency)}</div>
-                                <div className={styles.amount}>{formatAmount(item.amount, item.currency)}</div>
+                            <div key={item.id} className={className}>
+                                <div className={styles.left}>
+                                    <div className={styles.top}>
+                                        <div className={styles.name}>{item.name}</div>
+                                        <div className={styles.current}>{formatAmount(current, '')}</div>
+                                        <div className={styles.total}>{formatAmount(total, '')}</div>
+                                    </div>
+                                    <div className={styles.progress}><div className={styles.fill} style={progressStyle}></div></div>
+                                </div>
+                                <div className={styles.right}><div className={styles.remains}>{formatAmount(remains, item.currency)}</div></div>
                             </div>
                         ); 
                     })
