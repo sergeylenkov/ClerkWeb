46291b5fdcc83e7e425cc0c3b180ac7d80c4b32d
Sergey Lenkov <serglazy@gmail.com>
2019-04-08 22:21:16 +0300

    Added transactions filter

--- client/src/components/pages/Transactions.js
+++ client/src/components/pages/Transactions.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TransactionsList } from './transactions/List.js';
+import { TransactionsFilter } from './transactions/Filter.js';
 
 import styles from './Transactions.module.css';
 
@@ -7,6 +8,7 @@ export class TransactionsPage extends React.Component {
     render() {
         return (
             <div className={styles.container}>
+                <TransactionsFilter />
                 <TransactionsList />
             </div>
         )
